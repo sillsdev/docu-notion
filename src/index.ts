cad77ac6298ee41c99ae526a3c7523709370a67d
Hatton <hattonjohn@gmail.com>
2022-05-18 10:18:28 -0600

    fix: make the two path parameters required

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,11 @@ program
     "-r, --root-page <string>",
     "the 31 character ID of the page which is the root of your notion docs"
   )
-  .option(
+  .requiredOption(
     "-m, --markdown-output-path  <string>",
     "root of the hierarchy for md files. WARNING: node-pull will delete this directory."
   )
-  .option(
+  .requiredOption(
     "-i, --img-output-path  <string>",
     "path to directory where images will be stored"
   );
