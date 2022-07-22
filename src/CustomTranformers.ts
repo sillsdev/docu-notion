c952f9d862b966537d3f44c85fd4ef39bc18de7c
Hatton <hattonjohn@gmail.com>
2022-07-22 10:48:17 -0600

    Support columns

--- src/CustomTranformers.ts
+++ src/CustomTranformers.ts
@@ -28,6 +28,9 @@ async function notionColumnListToMarkdown(
   notionClient: Client,
   block: ListBlockChildrenResponseResult
 ): Promise<string> {
+  // Enhance: The @notionhq/client, which uses the official API, cannot yet get at column formatting information (column_ratio)
+  // However https://github1s.com/NotionX/react-notion-x/blob/master/packages/react-notion-x/src/block.tsx#L528 can get it.
+
   const { id, has_children } = block as any; // "any" because the notion api type system is complex with a union that don't know how to help TS to cope with
 
   if (!has_children) return "";
@@ -40,7 +43,7 @@ async function notionColumnListToMarkdown(
 
   const columns: string[] = await Promise.all(column_list_promise);
 
-  return `<div class='column_list'>${columns.join("\n\n")}</div>`;
+  return `<div class='notion-row'>\n${columns.join("\n\n")}\n</div>`;
 }
 
 async function notionColumnToMarkdown(
@@ -48,6 +51,7 @@ async function notionColumnToMarkdown(
   notionClient: Client,
   block: ListBlockChildrenResponseResult
 ): Promise<string> {
+  console.log(JSON.stringify(block));
   const { id, has_children } = block as any; // "any" because the notion api type system is complex with a union that don't know how to help TS to cope with
 
   if (!has_children) return "";
@@ -60,7 +64,11 @@ async function notionColumnToMarkdown(
 
   const childrenStrings: string[] = await Promise.all(childrenPromise);
 
-  return `<div class='column'>${childrenStrings.join("\n\n")}</div>`;
+  // note: it would look better in the markup with \n, but that
+  // causes notion-to-md to give us ":::A" instead of \n for some reason.
+  return `<div class='notion-column'>\n\n${childrenStrings.join(
+    "\n\n"
+  )}\n\n</div>`;
 }
 
 async function getBlockChildren(
