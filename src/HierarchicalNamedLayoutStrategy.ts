b3c5f5b182e7d917fbe28da222bb85a3229e26f1
Hatton <hattonjohn@gmail.com>
2022-11-18 07:32:43 -0700

    comment update

--- src/HierarchicalNamedLayoutStrategy.ts
+++ src/HierarchicalNamedLayoutStrategy.ts
@@ -5,13 +5,7 @@ import { NotionPage } from "./NotionPage";
 
 // This strategy gives us a file tree that mirrors that of notion.
 // Each level in the outline becomes a directory, and each file bears the name of the Notion document.
-// Pros: Easy to read and understand. The URLs are perhaps better for SEO.
-// Cons: The URLs become fragile. Moving a file, enclosing it in a new level, or tweaking
-// the title means the url will change. This may or may not be worth it to you.
-//
-// Note that I tried getting a readable file tree while generating slugs based on the notion page id.
-// However Docusaurus still makes the url be the "hierarchical path"/slug. So the using the slug as an id
-// doesn't buy us much... it would give protection against name changes, but not changes to the outline structure.
+// As long as you use slugs, the urls is still just something like https://site/slug
 
 export class HierarchicalNamedLayoutStrategy extends LayoutStrategy {
   public newLevel(
