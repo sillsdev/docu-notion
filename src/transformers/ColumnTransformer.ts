da5ee8b4083f4290bbed922fa07476677e5bf16f
Andrew Polk <andrew_polk@sil.org>
2022-12-19 17:08:33 -0700

    feat: Allow for variable-width columns

--- src/transformers/ColumnTransformer.ts
+++ src/transformers/ColumnTransformer.ts
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+import { NotionAPI } from "notion-client";
 import { NotionToMarkdown } from "notion-to-md";
 import { ListBlockChildrenResponseResult } from "notion-to-md/build/types";
 import { getBlockChildren } from "./CustomTransformers";
@@ -21,9 +22,29 @@ export async function notionColumnToMarkdown(
 
   const childrenStrings: string[] = await Promise.all(childrenPromise);
 
+  const style = await getStyleInJsxFormat(id);
+
   // note: it would look better in the markup with \n, but that
   // causes notion-to-md to give us ":::A" instead of \n for some reason.
-  return `<div class='notion-column'>\n\n${childrenStrings.join(
+  return `<div class='notion-column' style=${style}>\n\n${childrenStrings.join(
     "\n\n"
-  )}\n\n</div>`;
+  )}\n\n</div>
+  <div className='notion-spacer' />`;
+}
+
+// This is a hack.
+// The official API doesn't give us access to the format information, including column_ratio.
+// So we use 'notion-client' which uses the unofficial API.
+// Once the official API gives us access to the format information, we can remove this
+// and the 'notion-client' dependency.
+async function getStyleInJsxFormat(blockId: string): Promise<string> {
+  const notion = new NotionAPI();
+  // Yes, it is odd to call 'getPage' for a block, but that's how we access the format info.
+  const recordMap = await notion.getPage(blockId);
+  const columnFormat = recordMap.block[blockId]?.value?.format as any;
+  const columnRatio = (columnFormat?.column_ratio as string) || 0.5;
+  const spacerWidth = `min(32px, 4vw)`; // This matches the value in css.
+  const width = `calc((100% - ${spacerWidth}) * ${columnRatio})`;
+  const style = `{{width:'${width}'}}`; // JSX
+  return style;
 }
