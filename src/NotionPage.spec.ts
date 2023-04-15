651f924deef6565400be4e52919fd4ed85c900b0
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-15 11:56:50 +0100

    Unit tested getPlanTextProperty

---
+++ src/NotionPage.spec.ts
@@ -0,0 +1,105 @@
+import { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
+import { NotionPage } from "./NotionPage";
+
+describe("NotionPage", () => {
+  const mockMetadata: GetPageResponse = {
+    object: "page",
+    id: "6e6921b9-b1f5-4614-ab3c-bf1a73358a1f",
+    created_time: "2023-04-11T10:17:00.000Z",
+    last_edited_time: "2023-04-13T20:24:00.000Z",
+    created_by: {
+      object: "user",
+      id: "USERID",
+    },
+    last_edited_by: {
+      object: "user",
+      id: "USERID",
+    },
+    cover: null,
+    icon: {
+      type: "file",
+      file: {
+        url: "https:/dummy_URL",
+        expiry_time: "2023-04-15T11:50:20.461Z",
+      },
+    },
+    parent: {
+      type: "workspace",
+      workspace: true,
+    },
+    archived: false,
+    properties: {
+      title: {
+        id: "title",
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "Foo",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Foo",
+            href: null,
+          },
+          {
+            type: "text",
+            text: {
+              content: "Bar",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Bar",
+            href: null,
+          },
+        ],
+      },
+    },
+    url: "https://www.notion.so/Site-docu-notion-PAGEID",
+  };
+
+  describe("getPlainTextProperty", () => {
+    it("should return the plain text value of a property", () => {
+      const page = new NotionPage({
+        layoutContext: "Test Context",
+        pageId: "123",
+        order: 1,
+        metadata: mockMetadata,
+        foundDirectlyInOutline: true,
+      });
+
+      const result = page.getPlainTextProperty("title", "");
+
+      expect(result).toBe("FooBar");
+    });
+
+    it("should return the default value if the property is not found", () => {
+      const page = new NotionPage({
+        layoutContext: "Test Context",
+        pageId: "123",
+        order: 1,
+        metadata: mockMetadata,
+        foundDirectlyInOutline: true,
+      });
+
+      const result = page.getPlainTextProperty("nonexistent", "Default Value");
+
+      expect(result).toBe("Default Value");
+    });
+  });
+});
