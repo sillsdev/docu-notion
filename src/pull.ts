df657a7b12a38222fc10b6c6cf761533133a37dc
Dion Amago <dion@transition9.com>
2023-09-22 15:23:58 -0700

    3 -> 2 requests / second

--- src/pull.ts
+++ src/pull.ts
@@ -251,7 +251,7 @@ function writePage(page: NotionPage, finalMarkdown: string) {
 }
 
 const notionLimiter = new RateLimiter({
-  tokensPerInterval: 3,
+  tokensPerInterval: 2,
   interval: "second",
 });
 
